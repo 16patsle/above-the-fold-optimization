1314b6f2eca480c7a34ab7a8011fc02ec8ace6b1
Patrick Sletvold <patricksletvold@hotmail.com>
2018-08-16 11:06:02 +0200

    Fix conditional rendering of IntroView

--- admin/admin-app/src/index.js
+++ admin/admin-app/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import IntroView from './IntroView';
 //import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 if (document.getElementById('intro-root')) {
-    ReactDOM.render(<IntroView />, document.getElementById('intro-root'));
+    import('./IntroView').then(IntroView => {
+        ReactDOM.render(<IntroView />, document.getElementById('intro-root'));
+    })
 }
 //registerServiceWorker();
