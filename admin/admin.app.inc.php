17bc4f349f987daaa00adcce2a2a6025b37e4648
Patrick Sletvold <patricksletvold@hotmail.com>
2019-08-08 15:35:10 +0200

    Fix hidden attribute on wp_nonce fields

--- admin/admin.app.inc.php
+++ admin/admin.app.inc.php
@@ -8,7 +8,7 @@
 	<input id="wpabtf_uri" type="hidden" value="<?php print WPABTF_URI; ?>" />
 	<!-- HTML -->
 	<input id="admin_url_html_update" type="hidden" value="<?php echo admin_url('admin-post.php?action=abtf_html_update'); ?>" />
-	<div id="admin_nonce_html" type="hidden"><?php echo wp_nonce_field('abovethefold'); ?></div>
+	<div id="admin_nonce_html" hidden><?php echo wp_nonce_field('abovethefold'); ?></div>
 	<?php
 		$html_minify = isset($options['html_minify']) && intval($options['html_minify']) === 1;
 		$html_comments = isset($options['html_comments']) && intval($options['html_comments']) === 1;
@@ -24,7 +24,7 @@
 	} ?>"  />
 	<!-- CSS -->
 	<input id="admin_url_css_update" type="hidden" value="<?php echo admin_url('admin-post.php?action=abtf_css_update'); ?>" />
-	<div id="admin_nonce_css" type="hidden"><?php echo wp_nonce_field('abovethefold'); ?></div>
+	<div id="admin_nonce_css" hidden><?php echo wp_nonce_field('abovethefold'); ?></div>
 	<?php
 		$cssdelivery = isset($options['cssdelivery']) && intval($options['cssdelivery']) === 1;
 		$loadcss_enhanced = isset($options['loadcss_enhanced']) && intval($options['loadcss_enhanced']) === 1;
@@ -119,7 +119,7 @@
 	<input id="font_theme_path"  type="hidden" value='<?php print htmlentities(str_replace(ABSPATH, '/', trailingslashit(get_stylesheet_directory()) . 'fonts/'), ENT_COMPAT, 'utf-8'); ?>'/>
 	<!-- Javascript -->
 	<input id="admin_url_javascript_update" type="hidden" value="<?php echo admin_url('admin-post.php?action=abtf_javascript_update'); ?>" />
-	<div id="admin_nonce_javascript" type="hidden"><?php echo wp_nonce_field('abovethefold'); ?></div>
+	<div id="admin_nonce_javascript" hidden><?php echo wp_nonce_field('abovethefold'); ?></div>
 	<?php
 	$jsProxy = (isset($options['js_proxy']) && intval($options['js_proxy']) === 1);
 
@@ -188,7 +188,7 @@
 	<input id="lazyload_plugins_url" type="hidden" value="<?php print admin_url('plugin-install.php?s=Lazy+Load+XT&tab=search&type=term'); ?>">
 	<!-- Proxy -->
 	<input id="admin_url_proxy_update" type="hidden" value="<?php echo admin_url('admin-post.php?action=abtf_proxy_update'); ?>" />
-	<div id="admin_nonce_proxy" type="hidden"><?php echo wp_nonce_field('abovethefold'); ?></div>
+	<div id="admin_nonce_proxy" hidden><?php echo wp_nonce_field('abovethefold'); ?></div>
 	<?php
 		// Javascript Proxy Enabled?
     	$jsProxy = (isset($options['js_proxy']) && intval($options['js_proxy']) === 1);
@@ -258,7 +258,7 @@
 	<input id="proxy_settings" type="hidden" value='<?php echo $proxy_settings ?>'/>
 	<!-- Settings -->
 	<input id="admin_url_settings_update" type="hidden" value="<?php echo admin_url('admin-post.php?action=abtf_settings_update'); ?>" />
-	<div id="admin_nonce_settings" type="hidden"><?php echo wp_nonce_field('abovethefold'); ?></div>
+	<div id="admin_nonce_settings" hidden><?php echo wp_nonce_field('abovethefold'); ?></div>
 	<?php
 		$settings_adminbar = !isset($options['adminbar']) || intval($options['adminbar']) === 1;
 		$settings_clear_pagecache = !isset($options['clear_pagecache']) || intval($options['clear_pagecache']) === 1;
@@ -285,7 +285,7 @@
 			$client_hashes = $json;
 		}
 	?>
-	<input id="client_hashes"  type="hidden" value='<?php echo $client_hashes ?>'/>
+	<input id="client_hashes" type="hidden" value='<?php echo $client_hashes ?>'/>
 	<!-- Monitor -->
 	<input id="ssl_installed" type="hidden" value='<?php (strtolower($home_url['scheme']) === 'http') ? false : true; ?>'/>
 	<input id="admin_url_monitor_update" type="hidden" value="<?php echo admin_url('admin-post.php?action=abtf_monitor_update'); ?>" />
