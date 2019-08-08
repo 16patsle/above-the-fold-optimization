31466ddfef804f488fa76ffa0200faf2f05a76b1
Patrick Sletvold <patricksletvold@hotmail.com>
2019-08-08 20:25:33 +0200

    Add return JSDoc to getOption

--- admin/admin-app/src/utils/optionUtils.js
+++ admin/admin-app/src/utils/optionUtils.js
@@ -2,6 +2,7 @@
  * Get the value of an option from the state object. Value of this must be bound to React component.
  * @param {String} a - The option name, if no prefix is used, otherwise it's the prefix
  * @param {String} [b] - The option name if a prefix is used, otherwise not necessary
+ * @returns {Object} The value of the option requested.
  * */
 export function getOption(a, b) {
     let prefix = ''
