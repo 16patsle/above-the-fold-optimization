e7048113c8d2c7a9c2620364ba5c3ecf3a3f84f4
Patrick Sletvold <patricksletvold@hotmail.com>
2019-08-08 17:21:13 +0200

    Refactor ProxyView to use the options helper functions

--- admin/admin-app/src/ProxyView.js
+++ admin/admin-app/src/ProxyView.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { __ } from '@wordpress/i18n';
+import { getOption, toggleOption } from './utils/optionUtils';
+import newlineArrayString from './utils/newLineArrayString';
 import SettingCheckbox from './SettingCheckbox';
 import SettingTextarea from './SettingTextarea';
-import SubmitButton from './SubmitButton';
-import newlineArrayString from './utils/newLineArrayString';
 import SettingSelect from './SettingSelect';
 import SettingTextInput from './SettingTextInput';
+import SubmitButton from './SubmitButton';
 
 const homeUrl = window.homeUrl;
 
@@ -13,46 +14,24 @@ class ProxyView extends Component {
 	constructor(props) {
 		super(props);
 
-		this.proxySettings = JSON.parse(document.querySelector('#proxy_settings').value);
-
 		this.state = {
-			jsProxy: this.proxySettings.jsProxy,
-			jsProxyInclude: this.proxySettings.jsProxyInclude,
-			jsProxyExclude: this.proxySettings.jsProxyExclude,
-			jsProxyPreload: this.proxySettings.jsProxyPreload,
-			cssProxy: this.proxySettings.cssProxy,
-			cssProxyInclude: this.proxySettings.cssProxyInclude,
-			cssProxyExclude: this.proxySettings.cssProxyExclude,
-			cssProxyPreload: this.proxySettings.cssProxyPreload,
-			proxyCDN: this.proxySettings.proxyCDN,
-			proxyURL: this.proxySettings.proxyURL,
+			options: JSON.parse(document.querySelector('#proxy_settings').value),
 			cacheStats: {
-				files:'',
-				size:'',
-				date:''
+				files: '',
+				size: '',
+				date: ''
 			}
 		}
 
 		this.lgcode = document.querySelector('#lgcode').value;
 		this.google_intlcode = document.querySelector('#google_intlcode').value;
 
-		this.handleOptionToggle = this.handleOptionToggle.bind(this);
+		this.getOption = getOption.bind(this);
+		this.toggleOption = toggleOption.bind(this);
 	}
 
 	componentDidMount() {
 		this.updateCacheStats()
-	}
-
-	handleOptionToggle(option) {
-		this.setState({ [option]: !this.state[option] })
-	}
-
-	setOption(option, value) {
-		this.setState({ [option]: value })
-	}
-
-	getOption(option) {
-		return this.state[option];
 	}
 
 	async updateCacheStats() {
@@ -86,7 +65,7 @@ class ProxyView extends Component {
 										<p>This feature enables to pass the <a href={`https://developers.google.com/speed/docs/insights/LeverageBrowserCaching?hl=${this.lgcode}`} target="_blank">Leverage browser caching</a> rule from Google PageSpeed Insights.</p>
 			              				<table className="form-table">
 			                				<tbody>
-												<SettingCheckbox header="Proxy Scripts" name="abovethefold[js_proxy]" defaultChecked={this.proxySettings.jsProxy} onChange={this.handleOptionToggle.bind(this, 'jsProxy')} label="Enabled" description="Capture external scripts and load the scripts through a caching proxy."></SettingCheckbox>
+												<SettingCheckbox header="Proxy Scripts" name="abovethefold[js_proxy]" defaultChecked={this.getOption('jsProxy')} onChange={this.toggleOption.bind(this, 'jsProxy')} label="Enabled" description="Capture external scripts and load the scripts through a caching proxy."></SettingCheckbox>
 												{this.getOption('jsProxy') ?
 													<tr valign="top" className="proxyjsoptions">
 														<td colSpan="2" style={{ paddingTop: "0px" }}>
@@ -95,16 +74,16 @@ class ProxyView extends Component {
 																<div className="inside">
 																	<table className="form-table">
 																		<tbody>
-																			<SettingTextarea header="Proxy Include List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[js_proxy_include]" defaultValue={newlineArrayString(this.proxySettings.jsProxyInclude)} placeholder="Leave blank to proxy all external scripts..." description={<span>Enter (parts of) external javascript files to proxy, e.g. <code>google-analytics.com/analytics.js</code> or <code>facebook.net/en_US/sdk.js</code>. One script per line.</span>}></SettingTextarea>
-																			<SettingTextarea header="Proxy Exclude List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[js_proxy_exclude]" defaultValue={newlineArrayString(this.proxySettings.jsProxyExclude)} description={<span>Enter (parts of) external javascript files to exclude from the proxy. One script per line.</span>}></SettingTextarea>
-																			<SettingTextarea header="Proxy Preload List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[js_proxy_preload]" defaultValue={this.proxySettings.jsProxyPreload !== '' ? this.proxySettings.jsProxyPreload : null} description={<span>Enter the exact url or JSON config object [<a href="#jsoncnf" onClick={function(e){e.preventDefault();document.querySelector('#jsoncnf').scrollIntoView()}} title="More information">?</a>] of external scripts to preload for "script injected" async script capture, e.g. <code>https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js</code>. This setting will enable the proxy to load the cache url instead of the WordPress PHP proxy url. One url or JSON object per line.</span>}></SettingTextarea>
+																			<SettingTextarea header="Proxy Include List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[js_proxy_include]" defaultValue={newlineArrayString(this.getOption('jsProxyInclude'))} placeholder="Leave blank to proxy all external scripts..." description={<span>Enter (parts of) external javascript files to proxy, e.g. <code>google-analytics.com/analytics.js</code> or <code>facebook.net/en_US/sdk.js</code>. One script per line.</span>}></SettingTextarea>
+																			<SettingTextarea header="Proxy Exclude List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[js_proxy_exclude]" defaultValue={newlineArrayString(this.getOption('jsProxyExclude'))} description={<span>Enter (parts of) external javascript files to exclude from the proxy. One script per line.</span>}></SettingTextarea>
+																			<SettingTextarea header="Proxy Preload List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[js_proxy_preload]" defaultValue={this.getOption('jsProxyPreload') !== '' ? this.getOption('jsProxyPreload') : null} description={<span>Enter the exact url or JSON config object [<a href="#jsoncnf" onClick={function(e){e.preventDefault();document.querySelector('#jsoncnf').scrollIntoView()}} title="More information">?</a>] of external scripts to preload for "script injected" async script capture, e.g. <code>https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js</code>. This setting will enable the proxy to load the cache url instead of the WordPress PHP proxy url. One url or JSON object per line.</span>}></SettingTextarea>
 																		</tbody>
 																	</table>
 																</div>
 															</div>
 														</td>
 													</tr> : null}
-												<SettingCheckbox header="Proxy Stylesheets" name="abovethefold[css_proxy]" defaultChecked={this.proxySettings.cssProxy} onChange={this.handleOptionToggle.bind(this, 'cssProxy')} label="Enabled" description="Capture external stylesheets and load the files through a caching proxy."></SettingCheckbox>
+												<SettingCheckbox header="Proxy Stylesheets" name="abovethefold[css_proxy]" defaultChecked={this.getOption('cssProxy')} onChange={this.toggleOption.bind(this, 'cssProxy')} label="Enabled" description="Capture external stylesheets and load the files through a caching proxy."></SettingCheckbox>
 												{this.getOption('cssProxy') ?
 													<tr valign="top" className="proxycssoptions">
 														<td colSpan="2" style={{ paddingTop: "0px" }}>
@@ -113,9 +92,9 @@ class ProxyView extends Component {
 																<div className="inside">
 																	<table className="form-table">
 																		<tbody>
-																			<SettingTextarea header="Proxy Include List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[css_proxy_include]" defaultValue={newlineArrayString(this.proxySettings.cssProxyInclude)} placeholder="Leave blank to proxy all external stylesheets..." description={<span>Enter (parts of) external stylesheets to proxy, e.g. <code>googleapis.com/jquery-ui.css</code>. One stylesheet per line.</span>}></SettingTextarea>
-																			<SettingTextarea header="Proxy Exclude List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[css_proxy_exclude]" defaultValue={newlineArrayString(this.proxySettings.cssProxyExclude)} description={<span>Enter (parts of) external stylesheets to exclude from the proxy. One stylesheet per line.</span>}></SettingTextarea>
-																			<SettingTextarea header="Proxy Preload List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[css_proxy_preload]" defaultValue={this.proxySettings.cssProxyPreload !== '' ? this.proxySettings.cssProxyPreload : null} description={<span>Enter the exact url or JSON config object of external stylesheets to preload for "script injected" async stylesheet capture, e.g. <code>http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css</code>. This setting will enable the proxy to load the cache url instead of the WordPress PHP proxy url. One url or JSON object per line.</span>}></SettingTextarea>
+																			<SettingTextarea header="Proxy Include List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[css_proxy_include]" defaultValue={newlineArrayString(this.getOption('cssProxyInclude'))} placeholder="Leave blank to proxy all external stylesheets..." description={<span>Enter (parts of) external stylesheets to proxy, e.g. <code>googleapis.com/jquery-ui.css</code>. One stylesheet per line.</span>}></SettingTextarea>
+																			<SettingTextarea header="Proxy Exclude List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[css_proxy_exclude]" defaultValue={newlineArrayString(this.getOption('cssProxyExclude'))} description={<span>Enter (parts of) external stylesheets to exclude from the proxy. One stylesheet per line.</span>}></SettingTextarea>
+																			<SettingTextarea header="Proxy Preload List" style={{ width: '100%', height: 50, fontSize: 11 }} name="abovethefold[css_proxy_preload]" defaultValue={this.getOption('cssProxyPreload') !== '' ? this.getOption('cssProxyPreload') : null} description={<span>Enter the exact url or JSON config object of external stylesheets to preload for "script injected" async stylesheet capture, e.g. <code>http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css</code>. This setting will enable the proxy to load the cache url instead of the WordPress PHP proxy url. One url or JSON object per line.</span>}></SettingTextarea>
 																		</tbody>
 																	</table>
 																</div>
@@ -135,8 +114,8 @@ class ProxyView extends Component {
 			                			      			</fieldset>
 			                			    		</td>
 			                			  		</tr>
-												<SettingTextInput header="Proxy CDN" type="url" style={{ width: '100%' }} name="abovethefold[proxy_cdn]" defaultValue={this.proxySettings.proxyCDN} placeholder="Leave blank for the default WordPress (plugin modified) upload directory url..." description={<span>Enter the default CDN url for cached resources, e.g. <code><strong>https://cdn.domain.com</strong>/wp-content/uploads/abovethefold/proxy/.../resource.js</code>. You can set a custom CDN per individual resource using a JSON config object.</span>}></SettingTextInput>
-			                			  		<SettingTextInput header="Proxy URL" type="url" style={{ width: '100%' }} name="abovethefold[proxy_url]" defaultValue={this.proxySettings.proxyURL} placeholder="Leave blank for the default WordPress PHP based proxy url..." description={<span>Enter a custom proxy url to serve captured external resources. There are 2 parameters that can be used in the url: <code>{'{'}PROXY:URL{'}'}</code> and <code>{'{'}PROXY:TYPE{'}'}</code>.</span>}><p className="description">E.g.: <code>https://nginx-proxy.mydomain.com/{'{'}PROXY:TYPE{'}'}/{'{'}PROXY:URL{'}'}</code>. Type is the string <u>js</u> or <u>css</u>.</p></SettingTextInput>
+												<SettingTextInput header="Proxy CDN" type="url" style={{ width: '100%' }} name="abovethefold[proxy_cdn]" defaultValue={this.getOption('proxyCDN')} placeholder="Leave blank for the default WordPress (plugin modified) upload directory url..." description={<span>Enter the default CDN url for cached resources, e.g. <code><strong>https://cdn.domain.com</strong>/wp-content/uploads/abovethefold/proxy/.../resource.js</code>. You can set a custom CDN per individual resource using a JSON config object.</span>}></SettingTextInput>
+												<SettingTextInput header="Proxy URL" type="url" style={{ width: '100%' }} name="abovethefold[proxy_url]" defaultValue={this.getOption('proxyURL')} placeholder="Leave blank for the default WordPress PHP based proxy url..." description={<span>Enter a custom proxy url to serve captured external resources. There are 2 parameters that can be used in the url: <code>{'{'}PROXY:URL{'}'}</code> and <code>{'{'}PROXY:TYPE{'}'}</code>.</span>}><p className="description">E.g.: <code>https://nginx-proxy.mydomain.com/{'{'}PROXY:TYPE{'}'}/{'{'}PROXY:URL{'}'}</code>. Type is the string <u>js</u> or <u>css</u>.</p></SettingTextInput>
 			                				</tbody>
 										</table>
 			              				<hr />
