6bcdae8855814118a6c020a4e5fade06de8db653
Patrick Sletvold <patricksletvold@hotmail.com>
2019-08-10 19:55:16 +0200

    Add a utility for decoding html entities

---
+++ admin/admin-app/src/utils/htmlDecode.js
@@ -0,0 +1,22 @@
+/**
+ * Decode HTML entities in a string
+ * 
+ * https://stackoverflow.com/a/4835406/4285005
+ * 
+ * @param {String} text - The HTML string to decode
+ * @returns {String} The decoded string
+ */
+export default function htmlDecode(text) {
+  if(typeof text !== "string"){
+    return text
+  }
+  var map = {
+    '&amp;': '&',
+    '&lt;': '<',
+    '&gt;': '>',
+    '&quot;': '"',
+    '&#039;': "'"
+  };
+
+  return text.replace(/(&amp;|&lt;|&gt;|&quot;|&#039;)/g, function (m) { return map[m]; });
+}
