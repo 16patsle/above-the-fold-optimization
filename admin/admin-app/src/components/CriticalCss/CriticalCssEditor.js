7db25676167c731cdc94374fec6db4a273cce7cc
Patrick Sletvold <patricksletvold@hotmail.com>
2020-03-15 11:46:49 +0100

    Extract critical css editor and editor ui into own component

---
+++ admin/admin-app/src/components/CriticalCss/CriticalCssEditor.js
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import { __ } from '@wordpress/i18n';
+import { lgCode, utmString } from '../../utils/globalVars';
+import CssEditor from '../CssEditor';
+import sizeFormat from '../../utils/sizeFormat';
+import byteCount from '../../utils/byteCount';
+import './CriticalCssSettings.css';
+
+const CriticalCssEditor = props => {
+  const [editorLoaded, setEditorLoaded] = useState(false);
+
+  const [showEditor, setShowEditor] = useState(true);
+
+  return (
+    <li
+      className={
+        'menu-item menu-item-depth-0 menu-item-page pending ' +
+        (editorLoaded ? 'menu-item-edit-active' : 'menu-item-edit-inactive')
+      }
+      style={{ display: 'list-item', position: 'relative', top: '0px' }}
+    >
+      <div
+        className="menu-item-bar criticalcss-edit-header"
+        onClick={e => {
+          e.preventDefault();
+          setShowEditor(!showEditor);
+        }}
+      >
+        <div
+          className="menu-item-handle"
+          style={{ width: 'auto', cursor: 'pointer' }}
+        >
+          <span className="item-title" title={props.itemTitle}>
+            <span className="menu-item-title">{props.title}</span>
+            <span className="is-submenu">
+              {props.link.value.trim() !== '' ? (
+                <span>{sizeFormat(byteCount(props.link.value), 2)}</span>
+              ) : (
+                <span style={{ color: '#f1b70a' }}>{__('empty', 'abtfr')}</span>
+              )}
+              {props.config && (
+                <span style={{ float: 'right' }}>
+                  {__('Weight:', 'abtfr')}{' '}
+                  {props.config.weight !== undefined ? props.config.weight : 1}
+                </span>
+              )}
+            </span>
+          </span>
+          {!editorLoaded && (
+            <span className="is-submenu loading-editor">
+              <span style={{ color: '#ea4335' }}>Loading editor...</span>
+            </span>
+          )}
+          <span className="item-controls">
+            {props.config && (
+              <button
+                type="button"
+                className="item-delete button button-small button-del"
+                title="Delete conditional Critical CSS"
+                data-confirm="<?php echo htmlentities(__('Are you sure you want to delete this conditional Critical CSS?', 'abtfr'), ENT_COMPAT, 'utf-8'); ?>"
+              >
+                ✗
+              </button>
+            )}
+            <button type="button" className="item-edit">
+              ^
+            </button>
+          </span>
+        </div>
+      </div>
+
+      <div
+        id="ccss_editor_global"
+        className="ccss_editor"
+        style={showEditor ? {} : { display: 'none' }}
+      >
+        {props.advancedEditor ? (
+          <>
+            <CssEditor
+              link={props.link}
+              editorDidMount={() => {
+                setEditorLoaded(true);
+              }}
+            />
+            <input
+              type="hidden"
+              name={props.name}
+              id="abtfrcss"
+              value={props.link.value}
+            />
+          </>
+        ) : (
+          <textarea
+            className="abtfrcss"
+            id="abtfrcss"
+            name={props.name}
+            value={props.link.value}
+            onChange={e => props.link.set(e.target.value)}
+          />
+        )}
+        <div className="criticalcss-buttons">
+          <a
+            href={`https://www.google.com/search?q=beautify+css+online&hl=${lgCode}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button button-secondary button-small"
+          >
+            Beautify
+          </a>
+          <a
+            href={`https://www.google.com/search?q=minify+css+online&hl=${lgCode}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button button-secondary button-small"
+          >
+            Minify
+          </a>
+          <a
+            href="https://jigsaw.w3.org/css-validator/#validate_by_input+with_options"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button button-secondary button-small"
+          >
+            Validate
+          </a>
+          <a
+            href={`http://csslint.net/#${utmString}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button button-secondary button-small"
+          >
+            CSS
+            <span
+              style={{
+                color: '#768c1c',
+                fontWeight: 'bold',
+                marginLeft: '1px'
+              }}
+            >
+              LINT
+            </span>
+          </a>
+        </div>
+        <div
+          style={{
+            clear: 'both',
+            height: '1px',
+            overflow: 'hidden',
+            fontSize: '1px'
+          }}
+        >
+          &nbsp;
+        </div>
+        <ul className="menu-item-transport" />
+        {props.children}
+      </div>
+    </li>
+  );
+};
+
+export default CriticalCssEditor;
--- admin/admin-app/src/components/CriticalCss/CriticalCssSettings.js
+++ admin/admin-app/src/components/CriticalCss/CriticalCssSettings.js
@@ -2,15 +2,13 @@ import React, { useState } from 'react';
 import { __, sprintf } from '@wordpress/i18n';
 import useSWR from 'swr';
 import useLinkState from '../../utils/useLinkState';
-import { adminUrl, lgCode, utmString } from '../../utils/globalVars';
+import { adminUrl } from '../../utils/globalVars';
 import Info from '../Info';
 import SettingCheckbox from '../SettingCheckbox';
 import SubmitButton from '../SubmitButton';
-import CssEditor from '../CssEditor';
 import getSettings, { getJSON } from '../../utils/getSettings';
-import sizeFormat from '../../utils/sizeFormat';
-import byteCount from '../../utils/byteCount';
 import './CriticalCssSettings.css';
+import CriticalCssEditor from './CriticalCssEditor';
 
 const CriticalCssSettings = () => {
   const [options, , setOptions, linkOptionState] = useLinkState();
@@ -26,10 +24,6 @@ const CriticalCssSettings = () => {
       return getJSON('criticalcss');
     }
   );
-
-  const [editorLoaded, setEditorLoaded] = useState(false);
-
-  const [showEditor, setShowEditor] = useState(true);
 
   if (error || criticalCssError) {
     return (
@@ -56,195 +50,76 @@ const CriticalCssSettings = () => {
   }
 
   return (
-    <li
-      className={
-        'menu-item menu-item-depth-0 menu-item-page pending ' +
-        (editorLoaded ? 'menu-item-edit-active' : 'menu-item-edit-inactive')
-      }
-      style={{ display: 'list-item', position: 'relative', top: '0px' }}
+    <CriticalCssEditor
+      link={{ value: criticalCss, set: setCriticalCss }}
+      title={__('Global', 'abtfr')}
+      name="abtfr[css]"
+      advancedEditor={getOption('csseditor')}
     >
-      <div
-        className="menu-item-bar criticalcss-edit-header"
-        onClick={e => {
-          e.preventDefault();
-          setShowEditor(!showEditor);
-        }}
-      >
-        <div
-          className="menu-item-handle"
-          style={{ width: 'auto', cursor: 'pointer' }}
-        >
-          <span className="item-title">
-            <span className="menu-item-title">{__('Global', 'abtfr')}</span>
-            <span className="is-submenu">
-              {criticalCss.trim() !== '' ? (
-                <span>{sizeFormat(byteCount(criticalCss), 2)}</span>
-              ) : (
-                <span style={{ color: '#f1b70a' }}>{__('empty', 'abtfr')}</span>
-              )}
-            </span>
-          </span>
-          {!editorLoaded && (
-            <span className="is-submenu loading-editor">
-              <span style={{ color: '#ea4335' }}>Loading editor...</span>
-            </span>
-          )}
-          <span className="item-controls">
-            <button type="button" className="item-edit">
-              ^
-            </button>
-          </span>
-        </div>
-      </div>
-
-      <div
-        id="ccss_editor_global"
-        className="ccss_editor"
-        style={showEditor ? {} : { display: 'none' }}
-      >
-        {getOption('csseditor') ? (
-          <>
-            <CssEditor
-              link={{ value: criticalCss, set: setCriticalCss }}
-              editorDidMount={() => {
-                setEditorLoaded(true);
-              }}
-            />
-            <input
-              type="hidden"
-              name="abtfr[css]"
-              id="abtfrcss"
-              value={criticalCss}
-            />
-          </>
-        ) : (
-          <textarea
-            className="abtfrcss"
-            id="abtfrcss"
-            name="abtfr[css]"
-            value={criticalCss}
-            onChange={e => setCriticalCss(e.target.value)}
+      <table className="form-table">
+        <tbody>
+          <SettingCheckbox
+            name="abtfr[csseditor]"
+            header={__('Use advanced CSS editor', 'abtfr')}
+            link={linkOptionState('csseditor')}
+            label={__('Enabled', 'abtfr')}
+            description={
+              <>
+                Use the{' '}
+                <a
+                  href="https://microsoft.github.io/monaco-editor/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Monaco editor
+                </a>{' '}
+                with CSS error reporting.
+              </>
+            }
           />
+          <SettingCheckbox
+            name="abtfr[http2_push_criticalcss]"
+            header={__('Push Critical CSS using HTTP/2 Server Push', 'abtfr')}
+            link={linkOptionState('http2PushCriticalcss')}
+            label={__('Enabled', 'abtfr')}
+            description={
+              <>
+                When enabled, the critical CSS is not inlined but instead pushed
+                together with the HTML (
+                <a
+                  href="https://developers.google.com/web/fundamentals/performance/http2/#server_push"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  documentation
+                </a>
+                ).
+              </>
+            }
+          />
+        </tbody>
+      </table>
+      <div style={{ marginTop: '1em' }}>
+        {getOption('http2PushCriticalcss') && (
+          <Info
+            color="yellow"
+            style={{
+              marginBottom: '1em'
+            }}
+          >
+            <strong>Note:</strong> When using this feature, make sure that your
+            server supports HTTP/2 Server Push. See the{' '}
+            <a href={`${adminUrl}admin.php?page=abtfr#/http2`}>HTTP/2-tab</a>{' '}
+            for more information.
+          </Info>
         )}
-        <div className="criticalcss-buttons">
-          <a
-            href={`https://www.google.com/search?q=beautify+css+online&hl=${lgCode}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="button button-secondary button-small"
-          >
-            Beautify
-          </a>
-          <a
-            href={`https://www.google.com/search?q=minify+css+online&hl=${lgCode}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="button button-secondary button-small"
-          >
-            Minify
-          </a>
-          <a
-            href="https://jigsaw.w3.org/css-validator/#validate_by_input+with_options"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="button button-secondary button-small"
-          >
-            Validate
-          </a>
-          <a
-            href={`http://csslint.net/#${utmString}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="button button-secondary button-small"
-          >
-            CSS
-            <span
-              style={{
-                color: '#768c1c',
-                fontWeight: 'bold',
-                marginLeft: '1px'
-              }}
-            >
-              LINT
-            </span>
-          </a>
-        </div>
-        <div
-          style={{
-            clear: 'both',
-            height: '1px',
-            overflow: 'hidden',
-            fontSize: '1px'
-          }}
-        >
-          &nbsp;
-        </div>
-        <ul className="menu-item-transport" />
-        <table className="form-table">
-          <tbody>
-            <SettingCheckbox
-              name="abtfr[csseditor]"
-              header={__('Use advanced CSS editor', 'abtfr')}
-              link={linkOptionState('csseditor')}
-              label={__('Enabled', 'abtfr')}
-              description={
-                <>
-                  Use the{' '}
-                  <a
-                    href="https://microsoft.github.io/monaco-editor/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Monaco editor
-                  </a>{' '}
-                  with CSS error reporting.
-                </>
-              }
-            />
-            <SettingCheckbox
-              name="abtfr[http2_push_criticalcss]"
-              header={__('Push Critical CSS using HTTP/2 Server Push', 'abtfr')}
-              link={linkOptionState('http2PushCriticalcss')}
-              label={__('Enabled', 'abtfr')}
-              description={
-                <>
-                  When enabled, the critical CSS is not inlined but instead
-                  pushed together with the HTML (
-                  <a
-                    href="https://developers.google.com/web/fundamentals/performance/http2/#server_push"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    documentation
-                  </a>
-                  ).
-                </>
-              }
-            />
-          </tbody>
-        </table>
-        <div style={{ marginTop: '1em' }}>
-          {getOption('http2PushCriticalcss') && (
-            <Info
-              color="yellow"
-              style={{
-                marginBottom: '1em'
-              }}
-            >
-              <strong>Note:</strong> When using this feature, make sure that
-              your server supports HTTP/2 Server Push. See the{' '}
-              <a href={`${adminUrl}admin.php?page=abtfr#/http2`}>HTTP/2-tab</a>{' '}
-              for more information.
-            </Info>
-          )}
-        </div>
-        <hr />
-        <SubmitButton type={['primary', 'large']} name="is_submit">
-          {__('Save', 'abtfr')}
-        </SubmitButton>
-        <div id="conditional">&nbsp;</div>
       </div>
-    </li>
+      <hr />
+      <SubmitButton type={['primary', 'large']} name="is_submit">
+        {__('Save', 'abtfr')}
+      </SubmitButton>
+      <div id="conditional">&nbsp;</div>
+    </CriticalCssEditor>
   );
 };
 
