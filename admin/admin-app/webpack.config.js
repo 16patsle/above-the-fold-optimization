d24346d7b232663d0c9bb9d425b99fbd4bb690b3
Patrick Sletvold <patricksletvold@hotmail.com>
2021-04-01 17:47:45 +0200

    Always do dependency extraction of React, needed for wp-components to work

--- admin/admin-app/webpack.config.js
+++ admin/admin-app/webpack.config.js
@@ -398,20 +398,7 @@ module.exports = function (webpackEnv) {
         fix: true,
         failOnError: isEnvProduction
       }),
-      isEnvProduction && new DependencyExtractionWebpackPlugin(),
-      isEnvDevelopment && new DependencyExtractionWebpackPlugin({
-        useDefaults: false,
-        requestToExternal: request => {
-          switch (request) {
-            case 'react':
-            case 'react-dom':
-              return undefined;
-            default:
-              return defaultRequestToExternal(request)
-          }
-        },
-        requestToHandle: defaultRequestToHandle
-      })
+      new DependencyExtractionWebpackPlugin(),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
