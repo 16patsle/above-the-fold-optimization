dce3b6198fb5295fb0e42d50b17fa78a31101e7a
Patrick Sletvold <patricksletvold@hotmail.com>
2021-04-02 14:47:18 +0200

    Add syntax highlighting and fix click to select in search replace example

--- admin/admin-app/src/components/SearchReplaceExample.js
+++ admin/admin-app/src/components/SearchReplaceExample.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import json from 'react-syntax-highlighter/dist/esm/languages/hljs/json';
+import vs from 'react-syntax-highlighter/dist/esm/styles/hljs/vs';
+import './SearchReplaceExample.css';
+
+SyntaxHighlighter.registerLanguage('json', json);
 
 class SearchReplaceExample extends Component {
   static defaultProps = {
@@ -20,10 +26,13 @@ class SearchReplaceExample extends Component {
       exampleHtml: this.props.children.string
     };
 
+    this.selectElement = React.createRef();
+
     this.handleClick = this.handleClick.bind(this);
+    this.handleClickSelect = this.handleClickSelect.bind(this);
   }
 
-  handleClick(e) {
+  handleClick() {
     if (typeof this.state.exampleHtml !== 'string') {
       this.setState({ exampleHtml: JSON.stringify(this.state.exampleHtml) });
     }
@@ -41,23 +50,42 @@ class SearchReplaceExample extends Component {
     }
   }
 
+  handleClickSelect() {
+    const selection = window.getSelection();
+
+    if (selection.rangeCount > 0) {
+      selection.removeAllRanges();
+    }
+
+    const range = document.createRange();
+    range.selectNode(this.selectElement.current.querySelector('code'));
+    selection.addRange(range);
+  }
+
   render() {
     return (
-      <span>
-        <strong>Example:</strong>
-        <code
-          className="clickselect"
+      <div className="searchreplaceexample" ref={this.selectElement}>
+        <strong>{__('Example:', 'abtfr')}</strong>
+        <SyntaxHighlighter
           title={this.props.title}
-          style={{ cursor: 'copy' }}
+          language="json"
+          style={vs}
+          customStyle={{
+            display: 'inline',
+            padding: 0,
+            background: 'none',
+            cursor: 'copy'
+          }}
+          onClick={this.handleClickSelect}
         >
           {this.state.exampleHtml}
-        </code>
+        </SyntaxHighlighter>
         (
         <Button isLink onClick={this.handleClick}>
           {this.state.showLinkContent}
         </Button>
         )
-      </span>
+      </div>
     );
   }
 }
