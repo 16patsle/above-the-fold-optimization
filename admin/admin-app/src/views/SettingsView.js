f129226ee7a9cf21e2b46b1c71f4f09cf040433e
Patrick Sletvold <patricksletvold@hotmail.com>
2021-04-02 16:43:21 +0200

    Fully localize SettingsView

--- admin/admin-app/src/views/SettingsView.js
+++ admin/admin-app/src/views/SettingsView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { __ } from '@wordpress/i18n';
+import { createInterpolateElement } from '@wordpress/element';
 import { ExternalLink } from '@wordpress/components';
 import useSettings from '../utils/useSettings';
 import { adminUrl, siteTitle, abtfrAdminNonce } from '../utils/globalVars';
@@ -8,6 +9,7 @@ import LoadingWrapper from '../components/LoadingWrapper';
 import PageContent from '../components/PageContent';
 import SettingCheckbox from '../components/SettingCheckbox';
 import SubmitButton from '../components/SubmitButton';
+import Info from '../components/Info';
 
 const SettingsView = () => {
   const { linkOptionState, getOption, shouldRender, error } = useSettings();
@@ -32,27 +34,31 @@ const SettingsView = () => {
                 header={__('Admin Bar', 'abtfr')}
                 name="abtfr[adminbar]"
                 link={linkOptionState('adminbar')}
-                description={
-                  <span>
-                    Show a <code>PageSpeed</code> menu in the top admin bar with
-                    links to website speed and security tests such as Google
-                    PageSpeed Insights.
-                  </span>
-                }
+                description={createInterpolateElement(
+                  __(
+                    'Show a <code>PageSpeed</code> menu in the top admin bar with links to website speed and security tests such as Google PageSpeed Insights.',
+                    'abtfr'
+                  ),
+                  {
+                    code: <code />
+                  }
+                )}
               />
               <SettingCheckbox
                 header={__('Clear Page Caches', 'abtfr')}
                 name="abtfr[clear_pagecache]"
                 link={linkOptionState('clearPagecache')}
-                description={
-                  <span>
-                    If enabled, the page related caches of{' '}
-                    <ExternalLink href="https://github.com/16patsle/abtf-reborn/tree/master/modules/plugins/">
-                      supported plugins
-                    </ExternalLink>{' '}
-                    is cleared when updating the above the fold settings.
-                  </span>
-                }
+                description={createInterpolateElement(
+                  __(
+                    'If enabled, the page related caches of <a>supported plugins</a> is cleared when updating the above the fold settings.',
+                    'abtfr'
+                  ),
+                  {
+                    a: (
+                      <ExternalLink href="https://github.com/16patsle/abtf-reborn/tree/master/modules/plugins/" />
+                    )
+                  }
+                )}
               />
               <SettingCheckbox
                 header={__('Debug Mode', 'abtfr')}
@@ -65,7 +71,7 @@ const SettingsView = () => {
               />
               {getOption('abtfOptionsExists') && (
                 <tr valign="top">
-                  <th scope="row">Import Settings</th>
+                  <th scope="row">{__('Import Settings', 'abtfr')}</th>
                   <td>
                     <SubmitButton name="import_settings_abtf">
                       {__('Import settings from ABTF', 'abtfr')}
@@ -91,24 +97,39 @@ const SettingsView = () => {
           <br />
           <h1>{__('Content Security Policy', 'abtfr')}</h1>
           <p>
-            Based on your current configuration, the ABTF Reborn inline client
-            javascript can be white listed using the following hashes. (
-            <ExternalLink href="https://content-security-policy.com/faq/">
-              documentation
-            </ExternalLink>
-            )
+            {createInterpolateElement(
+              __(
+                'Based on your current configuration, the ABTF Reborn inline client javascript can be white listed using the following hashes. (<a>documentation</a>)',
+                'abtfr'
+              ),
+              {
+                a: (
+                  <ExternalLink hhref="https://content-security-policy.com/faq/" />
+                )
+              }
+            )}
           </p>
-          <p className="warning_red">
-            <strong>Important:</strong> when you change the configuration of the
-            plugin the hashes may change.
-          </p>
+          <Info color="red">
+          {createInterpolateElement(
+              __(
+                '<strong>Important:</strong> when you change the configuration of the plugin the hashes may change.',
+                'abtfr'
+              ),
+              {
+                strong: <strong/>
+              }
+            )}
+            
+          </Info>
           {getOption('clientHashes') === 'false' ? (
-            <h3 className="warning_red">
+            <Info color="red">
+              <h3>
               {__(
                 'Failed to retrieve hashes. You can find the hashes in the Dev Tools console in the Chrome browser.',
                 'abtfr'
               )}
             </h3>
+            </Info>
           ) : (
             <span>
               <strong>{__('Public client', 'abtfr')}</strong>
