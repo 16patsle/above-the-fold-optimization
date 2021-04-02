3e80ec8c261d2dc8e561d692cb9fc54f9d065898
Patrick Sletvold <patricksletvold@hotmail.com>
2021-04-02 16:35:14 +0200

    Remove unused service worker stuff

--- admin/admin-app/src/index.js
+++ admin/admin-app/src/index.js
@@ -4,7 +4,6 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Loading from './components/Loading';
-//import registerServiceWorker from './registerServiceWorker';
 
 const HashRouter = React.lazy(() => import('react-router-dom/HashRouter'));
 const App = React.lazy(() => import('./App'));
