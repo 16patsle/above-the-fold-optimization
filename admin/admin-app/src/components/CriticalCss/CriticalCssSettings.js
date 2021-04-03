96ed6e92b0285b68299213f37083323723109b01
Patrick Sletvold <patricksletvold@hotmail.com>
2021-04-03 10:56:52 +0200

    Add loading indicator for main critical CSS

--- admin/admin-app/src/components/CriticalCss/CriticalCssSettings.js
+++ admin/admin-app/src/components/CriticalCss/CriticalCssSettings.js
@@ -9,6 +9,7 @@ import Info from '../Info';
 import SettingCheckbox from '../SettingCheckbox';
 import SubmitButton from '../SubmitButton';
 import CriticalCssEditor from './CriticalCssEditor';
+import Loading from '../Loading';
 
 const CriticalCssSettings = () => {
   const { linkOptionState, getOption, shouldRender, error } = useSettings();
@@ -31,64 +32,73 @@ const CriticalCssSettings = () => {
       shouldRender={!(!shouldRender || !shouldRenderCritical)}
       error={error || criticalCssError}
     >
-      <CriticalCssEditor
-        link={{ value: criticalCss, set: setCriticalCss }}
-        title={__('Global', 'abtfr')}
-        name="abtfr[css]"
-        advancedEditor={getOption('csseditor')}
-      >
-        <table className="form-table">
-          <tbody>
-            <SettingCheckbox
-              name="abtfr[csseditor]"
-              header={__('Use advanced CSS editor', 'abtfr')}
-              link={linkOptionState('csseditor')}
-              description={
-                <>
-                  Use the{' '}
-                  <ExternalLink href="https://microsoft.github.io/monaco-editor/">
-                    Monaco editor
-                  </ExternalLink>{' '}
-                  with CSS error reporting.
-                </>
-              }
-            />
-            <SettingCheckbox
-              name="abtfr[http2_push_criticalcss]"
-              header={__('Push Critical CSS using HTTP/2 Server Push', 'abtfr')}
-              link={linkOptionState('http2PushCriticalcss')}
-              description={
-                <>
-                  When enabled, the critical CSS is not inlined but instead
-                  pushed together with the HTML (
-                  <ExternalLink href="https://developers.google.com/web/fundamentals/performance/http2/#server_push">
-                    documentation
-                  </ExternalLink>
-                  ).
-                </>
-              }
-            />
-          </tbody>
-        </table>
-        {getOption('http2PushCriticalcss') && (
-          <div style={{ marginTop: '1em' }}>
-            <Info
-              color="yellow"
-              style={{
-                marginBottom: '1em'
-              }}
-            >
-              <strong>Note:</strong> When using this feature, make sure that
-              your server supports HTTP/2 Server Push. See the{' '}
-              <a href={`${adminUrl}admin.php?page=abtfr#/http2`}>HTTP/2-tab</a>{' '}
-              for more information.
-            </Info>
-          </div>
-        )}
-        <hr />
-        <SubmitButton />
-        <div id="conditional">&nbsp;</div>
-      </CriticalCssEditor>
+      {criticalCss !== '' ? (
+        <CriticalCssEditor
+          link={{ value: criticalCss, set: setCriticalCss }}
+          title={__('Global', 'abtfr')}
+          name="abtfr[css]"
+          advancedEditor={getOption('csseditor')}
+        >
+          <table className="form-table">
+            <tbody>
+              <SettingCheckbox
+                name="abtfr[csseditor]"
+                header={__('Use advanced CSS editor', 'abtfr')}
+                link={linkOptionState('csseditor')}
+                description={
+                  <>
+                    Use the{' '}
+                    <ExternalLink href="https://microsoft.github.io/monaco-editor/">
+                      Monaco editor
+                    </ExternalLink>{' '}
+                    with CSS error reporting.
+                  </>
+                }
+              />
+              <SettingCheckbox
+                name="abtfr[http2_push_criticalcss]"
+                header={__(
+                  'Push Critical CSS using HTTP/2 Server Push',
+                  'abtfr'
+                )}
+                link={linkOptionState('http2PushCriticalcss')}
+                description={
+                  <>
+                    When enabled, the critical CSS is not inlined but instead
+                    pushed together with the HTML (
+                    <ExternalLink href="https://developers.google.com/web/fundamentals/performance/http2/#server_push">
+                      documentation
+                    </ExternalLink>
+                    ).
+                  </>
+                }
+              />
+            </tbody>
+          </table>
+          {getOption('http2PushCriticalcss') && (
+            <div style={{ marginTop: '1em' }}>
+              <Info
+                color="yellow"
+                style={{
+                  marginBottom: '1em'
+                }}
+              >
+                <strong>Note:</strong> When using this feature, make sure that
+                your server supports HTTP/2 Server Push. See the{' '}
+                <a href={`${adminUrl}admin.php?page=abtfr#/http2`}>
+                  HTTP/2-tab
+                </a>{' '}
+                for more information.
+              </Info>
+            </div>
+          )}
+          <hr />
+          <SubmitButton />
+          <div id="conditional">&nbsp;</div>
+        </CriticalCssEditor>
+      ) : (
+        <Loading />
+      )}
     </LoadingWrapper>
   );
 };
